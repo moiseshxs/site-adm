c87dd32275a2341f91aba300a25d767f74c71860
moiseshxs <moiseshenryb@gmail.com>
2024-03-16 20:19:24 -0300

    Login e dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,116 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-</head>
-<body>
+    <head>
+        <title>Dashboard - Admin</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.3.3 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link href="assets/css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <main>
+            <section class="container-fluid vh-100 " style="background-color: #d9d9d9;">
+                <div class="row h-100">
+                    <div class="col-md-2 p-4 bg-white shadow">
+                        <ul class="nav flex-column">
+                            <li class="fw-medium my-1">
+                                <a class="link-dark" href="/dashboard" style="text-decoration: none;"><i class="fas fa-home"></i>
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li class="fw-medium my-1">
+                                <a class="link-dark" href="/produtos" style="text-decoration: none;"><i class="fa fa-address-card"></i>
+                                    Produtos
+                                </a>
+                            </li>
+                            <li class="fw-medium my-1">
+                                <a class="link-dark" href="/suporte" style="text-decoration: none;"><i class="fa fa-calendar"></i>
+                                    Suportes
+                                </a>
+                            </li>
+                            <li class="fw-medium my-1">
+                                <a class="link-dark" href="/cliente" style="text-decoration: none;"><i class="fa fa-comments"></i>
+                                    Clientes
+                                </a>
+                            </li>
+                            <li class="fw-medium my-1">
+                                <a class="link-dark" href="/config" style="text-decoration: none;"><i class="fa fa-users"></i>
+                                    Configurações
+                                </a>
+                            </li>
+                            <li class="fw-medium my-1">
+                                <a class="link-dark" href="../sair" style="text-decoration: none;"><i class="fa fa-user"></i>
+                                    Sair
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-10 p-3">
+                        <div class="row align-items-center ">
+                            <div class="col">
+                                <h2>Dashboard</h2>
+                            </div>
+                            <div class="col">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <div class="d-flex justify-content-end">
+                                            foto
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <span class="fw-semibold fs-5">Moises</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row h-75 mt-5 d-flex align-items-center justify-content-center">
+                            <div class="row h-50">
+                                <div class="col bg-primary">
+                                    
+                                    
+                                </div>
+                                <div class="col bg-danger">
+
+                                </div>
+                                <div class="col bg-primary">
+
+                                </div>
+                            </div>
+                            <div class="row h-50">
+                                <div class="col bg-danger">
+
+                                </div>
+                                <div class="col bg-primary">
     
-</body>
+                                </div>
+                                <div class="col bg-danger">
+    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="pt-br">
     <head>
-        <title>Login</title>
+        <title>Login - Admin</title>
         <!-- Required meta tags -->
         <meta charset="utf-8" />
         <meta
@@ -9,37 +9,36 @@
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
 
-        <!-- Bootstrap CSS v5.2.1 -->
+        <!-- Bootstrap CSS v5.3.3 -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link href="assets/css/style.css" rel="stylesheet">
     </head>
 
     <body>
         <main>
-            <section class="container-fluid vh-100 background">
+            <section class="container-fluid vh-100">
             <div class="row h-100 align-items-center">
-                <div class="col-7">
+                <div class="col">
                     <div class="d-flex align-items-center justify-content-center">
-                        <div class="border border-secondary shadow rounded-3 bg-light h-50 w-50 p-4">
+                        <div class="" style="width: 60%;">
+                        <h2 class="mb-5">Login</h2>
                             <form>
-                                <div class="mb-3">
-                                    <label for="email" class="form-label">Email:</label>
-                                    <input type="email" class="form-control" id="email">
+                                <div class="mb-4">
+                                    <input type="email" placeholder="Email" class="form-control shadow bg-secondary bg-opacity-10" id="email">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="senha" class="form-label">Senha:</label>
-                                    <input type="password" class="form-control" id="senha">
+                                <div class="mb-4">
+                                    <input type="password" placeholder="Senha" class="form-control shadow bg-secondary bg-opacity-10" id="senha">
                                 </div> 
                                 <div class="d-flex align-items-center justify-content-center">
                                     <!--<button class="btn btn-primary" href="/dashboard">Entrar</button>-->
-                                    <a class="btn botao" href="/dashboard">Entrar</a>
+                                    <a class="btn botao shadow" href="/dashboard">Entrar</a>
                                 </div>
                             </form>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-5 h-100 bg-danger">
+                <div class="col h-100 bg-danger">
                     <div class="d-flex h-100 align-items-center justify-content-center">
                        <h2>LOGO</h2> 
                     </div>
